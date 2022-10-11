c3763b2f4818a853b6f2160d0a769d39efc9fba1
Salinas Maximiliano <salinas.maximiliano.sebastian@gmail.com>
2022-10-11 16:36:04 -0300

    editado php

--- enviar.php
+++ enviar.php
@@ -1,29 +1,9 @@
 <?php
-// $name = $_POST['name'];
-// $mail = $_POST['mail'];
-// $phone = $_POST['phone'];
-// $message = $_POST['message'];
-
-// $message = "Este mensaje fue enviado por: " . $name . " \n";
-// $message .= "Su e-mail es: " . $mail . " \r\n";
-// $message .= "Teléfono de contacto: " . $phone . " \r\n";
-// $message .= "Mensaje: " . $_POST['message'] . " \r\n";
-// $message .= "Enviado el: " . date('d/m/Y', time());
-
-// $para = 'salinas.maximiliano.sebastian@gmail.com';
-// $asunto = 'Mensaje de Porfolio';
-
-// mail($para, $asunto, utf8_decode($message));
-
-// header("Location:index.html");
-
-    $name = $_POST['name'];
+ $name = $_POST['name'];
 	$mail = $_POST['mail'];
     $phone = $_POST['phone'];
 	$asunto = 'Formulario Rellenado';
 	$message = "Nombre: ".$name."<br> Email: $mail<br> Mensaje:".$_POST['mensaje'];
-
-
 	if(mail('salinas.maximiliano.sebastian@gmail.com', $asunto, $message)){
 		echo "Correo enviado";
 	}
